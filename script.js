3ec1404569bc8b0a68b92c5d50d5f93cc1b1f01b
Andrew Joia <drewdrewthis@gmail.com>
2015-10-13 09:05:42 -0400

    Starts to build out click event for submit button. Adds autofocus to input

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-function create(text) {
-	var node = document.createTextNode(text);   
-	document.getElementById("output").appendChild(node);
+function execute(n){
+	function create(text) {
+		var node = document.createTextNode(text);   
+		document.getElementById("output").appendChild(node);
+	};
+
+	for(var i=1;i<=n;i++) {
+
+		if(i%3==0) {
+			create("Fizz");
+		};
+		if(i%5==0){
+			create("Buzz");
+		};
+		if((i%3!=0) && (i%5!=0)) {
+			create(i);
+		};
+		var node = document.createElement("br");   
+		document.getElementById("output").appendChild(node);
+	};
 };
 
-for(var i=1;i<=100;i++) {
-
-	if(i%3==0) {
-		create("Fizz");
-	};
-	if(i%5==0){
-		create("Buzz");
-	};
-	if((i%3!=0) && (i%5!=0)) {
-		create(i);
-	};
-	var node = document.createElement("br");   
-	document.getElementById("output").appendChild(node);
-};
+document.getElementById('Submit').addEventListener('click', function(e){
+	e.preventDefault();
+	var userinput = document.getElementById('input').val();
+	var number = parseInt('userinput');
+	console.log("clicked!" + number);
+	execute(number);
+});
