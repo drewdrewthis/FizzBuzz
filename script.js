ff3b465fcb5e39271af7d4c4db89c1b6eb25c49b
Andrew Joia <drewdrewthis@gmail.com>
2015-10-12 20:56:02 -0400

    Pulls the node and child creation into a separate function

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+function create(text) {
+	var node = document.createTextNode(text);   
+	document.getElementById("output").appendChild(node);
+};
+
 for(var i=1;i<=100;i++) {
 
 	if(i%3==0) {
-		var node = document.createTextNode("Fizz");   
-		document.getElementById("output").appendChild(node);
-	}
+		create("Fizz");
+	};
 	if(i%5==0){
-		var node = document.createTextNode("Buzz");   
-		document.getElementById("output").appendChild(node);
-	}
+		create("Buzz");
+	};
 	if((i%3!=0) && (i%5!=0)) {
-		var node = document.createTextNode(i);   
-		document.getElementById("output").appendChild(node);
-	}
+		create(i);
+	};
 };
